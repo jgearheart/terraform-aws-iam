8aafd8b01d69c526a85119b87cdef1cce0195fdf
jgearheart <jeremiah.gearheart@gmail.com>
2019-08-12 15:38:20 -0400

    Update variables.tf

--- modules/iam-user/variables.tf
+++ modules/iam-user/variables.tf
@@ -1,28 +1,28 @@
 variable "create_user" {
   description = "Whether to create the IAM user"
-  type        = bool
-  default     = true
+
+  default     = "true"
 }
 
 variable "create_iam_user_login_profile" {
   description = "Whether to create IAM user login profile"
-  type        = bool
-  default     = true
+ 
+  default     = "true"
 }
 
 variable "create_iam_access_key" {
   description = "Whether to create IAM access key"
-  type        = bool
-  default     = true
+  
+  default     = "true"
 }
 
 variable "name" {
   description = "Desired name for the IAM user"
-  type        = string
+  type        = "string"
 }
 variable "empty_list" {
   description = "The length of the generated password"
-  type        = list
+  type        = "list"
   default = []
 }
 
@@ -34,8 +34,8 @@ variable "path" {
 
 variable "force_destroy" {
   description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
-  type        = bool
-  default     = false
+  
+  default     = "false"
 }
 
 variable "pgp_key" {
@@ -46,37 +46,37 @@ variable "pgp_key" {
 
 variable "password_reset_required" {
   description = "Whether the user should be forced to reset the generated password on first login."
-  type        = bool
-  default     = true
+  
+  default     = "true"
 }
 
 variable "password_length" {
   description = "The length of the generated password"
-  type        = number
+  type        = "number"
   default     = 20
 }
 
 variable "upload_iam_user_ssh_key" {
   description = "Whether to upload a public ssh key to the IAM user"
-  type        = bool
+  type        = "bool"
   default     = false
 }
 
 variable "ssh_key_encoding" {
   description = "Specifies the public key encoding format to use in the response. To retrieve the public key in ssh-rsa format, use SSH. To retrieve the public key in PEM format, use PEM"
-  type        = string
+  type        = "string"
   default     = "SSH"
 }
 
 variable "ssh_public_key" {
   description = "The SSH public key. The public key must be encoded in ssh-rsa format or PEM format"
-  type        = string
+  type        = "string"
   default     = ""
 }
 
 variable "permissions_boundary" {
   description = "The ARN of the policy that is used to set the permissions boundary for the user."
-  type        = string
+  type        = "string"
   default     = ""
 }
 
