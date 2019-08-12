9449fc7594c623e63db04df69a409101e8e81a41
jgearheart <jeremiah.gearheart@gmail.com>
2019-08-12 15:11:40 -0400

    Update outputs.tf

--- modules/iam-user/outputs.tf
+++ modules/iam-user/outputs.tf
@@ -1,6 +1,6 @@
 output "this_iam_user_name" {
   description = "The user's name"
-  value       = ${element(concat(aws_iam_user.this.*.name, [""]), 0)
+  value       = "${element(concat(aws_iam_user.this.*.name, [""]), 0)"
 }
 
 output "this_iam_user_arn" {
